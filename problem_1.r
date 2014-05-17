7bab39bb57233c88ea27d6f8aee392b728d7cd90
Brian Spiering <bspiering@gmail.com>
2014-05-17 05:13:07 -0700

    Add vectorized method
    Also cleaned up file

--- problem_1.r
+++ problem_1.r
@@ -8,15 +8,25 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 #
 # http://projecteuler.net/problem=1
+# => 233168
 
+# Setup
+cat("\014")  # Clear console
 rm(list=ls()) # Delete all variables
+stop_number <- 1000
 
-# Brute force
-stop_number = 1000
-sum = 0
+# Brute force method
+sum <- 0
 for (i in 1:stop_number-1) {
-  if (i %% 3 == 0 | i %% 5 ==0) {
-    sum = sum + i
+  if ((i %% 3 == 0) | (i %% 5 ==0)) {
+    sum <- sum + i
   }
 }
+
+# A vectorized method
+sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
+sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
+sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
+sum <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
+
 cat("The sum of all multiples of 3 or 5 below", stop_number, "is", sum, ".")
