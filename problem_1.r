a442b69bdb31cf2d281af83ddc5c22c6ddbb7a48
Brian Spiering <bspiering@gmail.com>
2014-05-17 13:43:19 -0700

    Add another vectorized method

--- problem_1.r
+++ problem_1.r
@@ -16,18 +16,24 @@ rm(list=ls()) # Delete all variables
 stop_number <- 1000
 
 # Brute force method -----------------------------------------------------------
-sum <- 0
+final_sum <- 0
 for (i in 1:stop_number-1) {
   if ((i %% 3 == 0) | (i %% 5 ==0)) {
-    sum <- sum + i
+    final_sum <- final_sum + i
   }
 }
 
-# # A vectorized method --------------------------------------------------------
-# sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
-# sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
-# sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
-# sum <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
+# A vectorized method ----------------------------------------------------------
+sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
+sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
+sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
+final_sum <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
+
+# Another vectorized method ----------------------------------------------------
+sequence <- (1:stop_number-1)
+final_sum <- (sum(sequence[sequence %% 3 == 0]) 
+              + sum(sequence[sequence %% 5 == 0])
+              - sum(sequence[sequence %% 15 == 0]))
 
 # Write out results ------------------------------------------------------------
-cat("The sum of all multiples of 3 or 5 below", stop_number, "is", sum, ".")
+cat("The sum of all multiples of 3 or 5 below", stop_number, "is", final_sum, ".")
