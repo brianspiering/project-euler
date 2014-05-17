08146a78321a5f7681f30341949066aae35456b1
Brian Spiering <bspiering@gmail.com>
2014-05-17 07:32:51 -0700

    Attempts for problem 2
    Also added headers

--- problem_1.r
+++ problem_1.r
@@ -10,12 +10,12 @@
 # http://projecteuler.net/problem=1
 # => 233168
 
-# Setup
-cat("\014")  # Clear console
+# Setup ------------------------------------------------------------------------
+cat("\014")   # Clear console
 rm(list=ls()) # Delete all variables
 stop_number <- 1000
 
-# Brute force method
+# Brute force method -----------------------------------------------------------
 sum <- 0
 for (i in 1:stop_number-1) {
   if ((i %% 3 == 0) | (i %% 5 ==0)) {
@@ -23,10 +23,11 @@ for (i in 1:stop_number-1) {
   }
 }
 
-# A vectorized method
-sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
-sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
-sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
-sum <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
+# # A vectorized method --------------------------------------------------------
+# sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
+# sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
+# sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
+# sum <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
 
+# Write out results ------------------------------------------------------------
 cat("The sum of all multiples of 3 or 5 below", stop_number, "is", sum, ".")
