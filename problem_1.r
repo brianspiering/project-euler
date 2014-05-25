802b6d5d1d77d1966e07b6362a6c4ccf91dc2fc1
Brian Spiering <bspiering@gmail.com>
2014-05-25 07:51:42 -0700

    Reorder methods based on memory use
    Additional minor improvements

--- problem_1.r
+++ problem_1.r
@@ -14,33 +14,36 @@ cat("\014")   # Clear console
 rm(list=ls()) # Delete all variables
 stop_number <- 1000
 
-# Brute force method -----------------------------------------------------------
+# A brute force method -----------------------------------------------------------
 final_sum_method_1 <- 0
 for (i in 1:stop_number-1) {
-  if ((i %% 3 == 0) || (i %% 5 ==0)) {
+  if ((i %% 3 == 0) || (i %% 5 == 0)) {
     final_sum_method_1 <- final_sum_method_1 + i
   }
 }
 
-# A vectorized method ----------------------------------------------------------
-sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
-sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
-sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
-final_sum_method_2 <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
-
-# Another vectorized method ----------------------------------------------------
+# A vectorized method ----------------------------------------------------
 sequence <- (1:stop_number-1)
 final_sum_method_3 <- (sum(sequence[sequence %% 3 == 0]) 
                      + sum(sequence[sequence %% 5 == 0])
                     - sum(sequence[sequence %% 15 == 0]))
 
+# A vectorized method ----------------------------------------------------------
+# This method uses less memory by creating only the parts of the sequence needed.
+sum_numbers_mod_3 <- sum(seq(0, stop_number-1, 3))
+sum_numbers_mod_5 <- sum(seq(0, stop_number-1, 5))
+sum_numbers_mod_15 <- sum(seq(0, stop_number-1, 15))
+final_sum_method_2 <- sum_numbers_mod_3 + sum_numbers_mod_5 - sum_numbers_mod_15
+
 # Write out results -----------------------------------------------------------
 sums_from_all_methods <- c(final_sum_method_1,
                            final_sum_method_2, 
                            final_sum_method_3)
+
 if (length(unique(sums_from_all_methods)) == 1) {
   print_sum <- final_sum_method_1
-  cat("The sum of all multiples of 3 or 5 below", stop_number, "is", print_sum, ".")
+  cat("The sum of all multiples of 3 or 5 below", stop_number, 
+      "is", print_sum, ".")
 } else {
   stop("Check methods")
 }
