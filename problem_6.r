eb2f5f06f77ecbd9cb4761ebcd9c46329ebe8790
Brian Spiering <bspiering@gmail.com>
2014-05-30 08:57:15 -0700

    Add benchmarking

--- problem_6.r
+++ problem_6.r
@@ -20,29 +20,54 @@ cat("\014")         # Clear console
 rm(list=ls())       # Delete all variables
 
 # Define functions
+source("benchmark_method.r")
 square <- function(x) {
     x ^ 2
 }
 
 # Define variables
-n_terms <- 10 # 10 | 100
+n_terms <- 100 # 10 | 100
 
 # A brute force method ---------------------------------------------------------
-sum_of_squares <- 0
-sum_of_numbers <- 0
-
-for (i in 1:n_terms) {
-  sum_of_squares <- sum_of_squares + i^2 
-  sum_of_numbers <- sum_of_numbers + i
+calc_difference_sum_squares_method_1 <- function(n_terms){
+  sum_of_squares <- 0
+  sum_of_numbers <- 0
+  
+  for (i in 1:n_terms) {
+    sum_of_squares <- sum_of_squares + i^2 
+    sum_of_numbers <- sum_of_numbers + i
+  }
+  
+  square_of_sum <- sum_of_numbers^2
+  difference_sum_squares <- square_of_sum-sum_of_squares
+  
+  return(difference_sum_squares)
 }
 
-square_of_sum <- sum_of_numbers^2
-difference_sum_squares_method_1 <- square_of_sum-sum_of_squares
+# Calculate answer
+difference_sum_squares_method_1 <- calc_difference_sum_squares_method_1(n_terms)
+
+# Benchmark method
+benchmark_method(method_number=1, 
+                 method_function=calc_difference_sum_squares_method_1,
+                 method_argument=n_terms) 
 
 # A vectorized method ----------------------------------------------------------
-sum_of_squares_method_2 <- tail(cumsum(square(1:n_terms)), n=1)
-square_of_sum_method_2 <- sum_of_numbers^2
-difference_sum_squares_method_2 <- square_of_sum_method_2 - sum_of_squares_method_2
+calc_difference_sum_squares_method_2 <- function(n_terms){
+  sum_of_squares <- tail(cumsum(square(1:n_terms)), n=1)
+  square_of_sum <- tail(cumsum(1:n_terms), n=1)^2
+  difference_sum_squares <- square_of_sum - sum_of_squares
+
+  return(difference_sum_squares)
+}
+
+# Calculate answer
+difference_sum_squares_method_2 <- calc_difference_sum_squares_method_2(n_terms)
+
+# Benchmark method
+benchmark_method(method_number=2, 
+                 method_function=calc_difference_sum_squares_method_2,
+                 method_argument=n_terms) 
 
 # Write out results ------------------------------------------------------------
 results_from_all_methods <- c(difference_sum_squares_method_1,
@@ -50,7 +75,7 @@ results_from_all_methods <- c(difference_sum_squares_method_1,
 
 if (length(unique(results_from_all_methods)) == 1) {
   results_final <- results_from_all_methods[1]
-  cat("The difference between the sum of the squares of the first ", n_terms, 
+  cat("\nThe difference between the sum of the squares of the first ", n_terms, 
       " natural numbers and the square of the sum is ", results_final, ".", 
       sep="")
 } else {
