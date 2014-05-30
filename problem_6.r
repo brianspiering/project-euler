0137daf5699657916d594915c91b93100bf312fb
Brian Spiering <bspiering@gmail.com>
2014-05-30 08:23:58 -0700

    Add a brute force method

--- problem_6.r
+++ problem_6.r
@@ -8,6 +8,7 @@
 #    1^2 + 2^2 + ... + 10^2 = 385
 # The square of the sum of the first ten natural numbers is,
 #   (1 + 2 + ... + 10)^2 = 55^2 = 3025
+#
 # Hence the difference between the sum of the squares of 
 # the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 #
@@ -17,14 +18,28 @@
 # Setup ------------------------------------------------------------------------
 cat("\014")         # Clear console
 rm(list=ls())       # Delete all variables
-stop_number <- 10
-difference <- NA
+
+# Define variables
+stop_number <- 100 # 10 | 100
 
 # A brute force method -----------------------------------------------------
+sum_of_squares <- 0
+sum_of_numbers <- 0
+
+for (i in 1:stop_number) {
+  sum_of_squares <- sum_of_squares + i^2 
+  sum_of_numbers <- sum_of_numbers + i
+}
+
+square_of_sum <- sum_of_numbers^2
+difference_sum_squares <- square_of_sum-sum_of_squares
+
+# Better ideas -----
+# try cumsum()
 
 # Write out results ------------------------------------------------------------
 cat("The difference between the sum of the squares of the first ", stop_number, 
-    " natural numbers and the square of the sum is ", difference, ".", 
+    " natural numbers and the square of the sum is ", difference_sum_squares, ".", 
     sep="")
 
-# =>
+# => 25164150
