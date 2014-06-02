c1d5d50c066e8abbd9ccc4aa3cb61a924dec5578
Brian Spiering <bspiering@gmail.com>
2014-06-02 10:14:48 -0700

    Clean up

--- python/test_problem_1.py
+++ python/test_problem_1.py
@@ -1,20 +1,21 @@
 import problem_1
 import unittest
 
-class KnownValues(unittest.TestCase):
+class TestProblem1(unittest.TestCase):
     knownValues = ( (10, 23), # n, sum_multiples
-                    (1000, 233168))
+                    (1000, 233168)) # 233168
 
-    def test_method_1_to_known_values(self):                           
+    def test_method_1_to_known_values(self):
         """method_1 should give known result with known input"""
-        for n, sum_multiples in self.knownValues:              
-            result = problem_1.sum_multiples_method_1(n)                     
+        for x, sum_multiples in self.knownValues:              
+            result = problem_1.sum_multiples_method_1(x)  
+            print(sum_multiples, result)                   
             self.assertEqual(sum_multiples, result)   
 
-    def test_method_2_to_known_values(self):                           
+    def test_method_2_to_known_values(self):
         """method_2 should give known result with known input"""
-        for n, sum_multiples in self.knownValues:              
-            result = problem_1.sum_multiples_method_2(n)                     
+        for x, sum_multiples in self.knownValues:              
+            result = problem_1.sum_multiples_method_2(x)                     
             self.assertEqual(sum_multiples, result)   
 
 if __name__ == '__main__':
