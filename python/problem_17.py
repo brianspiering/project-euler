cf51107b7dbe9878bd5a325516947a9e6d6a033e
Brian Spiering <bspiering@gmail.com>
2014-06-03 20:30:33 -0700

    Improve dictionary
    - Remove string keys
    - Add rest of numbers

--- python/problem_17.py
+++ python/problem_17.py
@@ -11,20 +11,21 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 
 n = 5 # 5 | 1000
 
-number_dict = { '1': 'one', # Arabic Number: English Word
-                '2': 'two',
-                '3': 'three',
-                '4': 'four',
-                '5': 'five'}
+# Arabic Number: English Word
+number_dict = { 1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',
+            8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',13:'thirteen',
+            14:'fourteen',15:'fifteen',16:'sixteen',17:'seventeen',
+            18:'eighteen',19:'nineteen',20:'twenty',30:'thirty',40:'forty',
+            50:'fifty',60:'sixty',70:'seventy',80:'eighty',90:'ninety',
+            100:'hundred',1000:'thousand'}
 
 def gen_letter_count(number_dict):
     "For each value in a given dict, count number of letters"
     return {_[0]:len(_[1]) for _ in number_dict.items()}
 
-
 def count_letters(n, letter_count_dict):
     "Count number of letters in words from 1 to n"
-    return sum([letter_count_dict[str(_)] for _ in xrange(1,n+1)])
+    return sum([letter_count_dict[_] for _ in xrange(1,n+1)])
 
 if __name__ == "__main__":
     letter_count_dict = gen_letter_count(number_dict)
