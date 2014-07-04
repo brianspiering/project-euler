d9a58d0a0296ba27e67f52cce33f61f4395ce1ec
Brian Spiering <bspiering@gmail.com>
2014-07-04 10:35:18 -0700

    More setup for problem

--- python/problem_476.py
+++ python/problem_476.py
@@ -13,10 +13,21 @@ You are given S(2) = R(1, 1, 1) ≈ 0.31998, S(5) ≈ 1.25899.
 
 Find S(1803) rounded to 5 decimal places behind the decimal point.
 """
+
+from math import pi, sqrt
+
+# Useful links
+# http://cms.math.ca/cmb/v4/cmb1961v04.0153-0155.pdf
+# Circles in equilateral triangle - http://www2.stetson.edu/~efriedma/cirintri/
+# Circles in arbitrary triangles - http://www2.stetson.edu/~efriedma/cirinttt/
+
 n = 2 # 2 | 5 | 1803
 
 def S(n):
-    return 0.31998
+    return round(0.31998,5)
+
+def area_of_circle(r):
+    return (r**2)*pi
 
 if __name__ == "__main__":
     print("S({0}) is {1}."
