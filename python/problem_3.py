d91a0308c3365f967adb9c03596267eaf7b23485
Brian Spiering <bspiering@gmail.com>
2014-07-04 08:57:18 -0700

    Initial commit

---
+++ python/problem_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+""" Solution to "Largest prime factor", aka Problem 3
+
+http://projecteuler.net/problem=3
+
+The prime factors of 13195 are 5, 7, 13 and 29.
+
+What is the largest prime factor of the number 600851475143 ?
+"""
+
+n = 13195  # 13195 | 600851475143
+
+largest_prime_factor = None
+
+if __name__ == "__main__":
+    print("The largest prime factor of {0} is {1}."
+            .format(n, largest_prime_factor))
