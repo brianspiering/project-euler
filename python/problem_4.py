4703ae282b590759534f1062ff1a0bc2c6420b0e
Brian Spiering <bspiering@gmail.com>
2014-07-05 10:41:17 -0700

    Brute force solution

--- python/problem_4.py
+++ python/problem_4.py
@@ -12,12 +12,20 @@ The largest palindrome made from the product of two 2-digit numbers is
 Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
-low, high = 10, 99  # 10, 99 | 100, 999
+low, high = 100, 999  # 10, 99 | 100, 999
 
-n = 9009  
+def find_largest_palindrome(low, high):
+    """(slightly) clever brute force method"""
+    largest_palindrome = 0
+    for x in xrange(high,low-1,-1):
+       for y in xrange(high,low-1,-1):
+            if is_palindrome(x*y) and (x*y > largest_palindrome):
+                largest_palindrome = x*y    
+    return largest_palindrome
 
-def is_palindrom(n):
+def is_palindrome(n):
     return str(n) == str(n)[::-1]
 
 if __name__ == "__main__":
-    print("The largest palindrome made from the product of two {0}-digit numbers is {1}.".format(len(str(low)), n))
+    print("The largest palindrome made from the product of two {0}-digit numbers is {1}."
+        .format(len(str(low)), find_largest_palindrome(low, high)))
