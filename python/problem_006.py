59b0161ca66f4eb7f3001b3490bdc8ac6006f16f
Brian Spiering <bspiering@gmail.com>
2014-07-06 20:29:12 -0700

    Solution using functional programming

--- python/problem_006.py
+++ python/problem_006.py
@@ -16,10 +16,13 @@ Hence the difference between the sum of the squares of the first ten natural num
 Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 """
 
-n = 10 # 10 | 100
+n = 100 # 10 | 100
 
 def find_sum_square_difference(n):
-    return 2640
+    "Use functional programming"
+    sum_of_squares = reduce(lambda x, y: x + y**2, xrange(1,n+1))
+    square_of_sum = (reduce(lambda x, y: x + y, xrange(1,n+1)))**2
+    return square_of_sum - sum_of_squares
 
 if __name__ == "__main__":
     print("The difference between the sum of the squares of the first one "
