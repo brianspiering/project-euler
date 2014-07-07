5ca21914066994b2b9912885bf47c85967cce324
Brian Spiering <bspiering@gmail.com>
2014-07-07 14:32:47 -0700

    Refactored - use islice to calculate exact value

--- python/problem_007.py
+++ python/problem_007.py
@@ -10,12 +10,14 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 """
 
+from itertools import islice
+
 n = 10001 # 6 | 10001
 
 ordinal_number_suffixes = {6:'th', 10001:'st'}
 
 def gen_primes():
-    "Generator function for prime numbers"
+    "A generator function for prime numbers"
     n = 2
     primes = set()
     while True:
@@ -27,7 +29,10 @@ def gen_primes():
             yield n
         n += 1
 
+def nth(iterable, n, default=None):
+    "Returns the nth item or a default value"
+    return next(islice(iterable, n, None), default)
+
 if __name__ == "__main__":
-    primes = gen_primes()
     print("The {0}{1} prime number is {2}."
-          .format(n, ordinal_number_suffixes[n], [primes.next() for _ in xrange(n)][-1]))
+          .format(n, ordinal_number_suffixes[n], nth(gen_primes(), n-1)))
