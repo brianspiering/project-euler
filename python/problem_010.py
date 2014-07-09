c18dd1f1cd01c9a964c9e428e9fea0c5893d4a18
Brian Spiering <bspiering@gmail.com>
2014-07-09 10:02:25 -0700

    Brute force solution

--- python/problem_010.py
+++ python/problem_010.py
@@ -10,11 +10,31 @@ The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 Find the sum of all the primes below two million.
 """
 
-n = 10 # 10 | 2000000
+n = 2000000 # 10 | 2000000
 
-def sum_primes(n):
-    return 17
+def gen_primes():
+    "A generator function for prime numbers"
+    n = 2
+    primes = set()
+    while True:
+        for p in primes:
+            if n % p == 0:
+                break
+        else:
+            primes.add(n)
+            yield n
+        n += 1
+
+def calc_sum_primes(n):
+    "Sum primes below n"
+    sum_primes = 0
+    primes = gen_primes()
+    current_prime = next(primes)
+    while current_prime < n:
+        sum_primes += current_prime
+        current_prime = next(primes)
+    return sum_primes
  
 if __name__ == "__main__":
     print("The sum of all the primes below {0} is {1}."
-          .format(n, sum_primes(n)))
+          .format(n, calc_sum_primes(n)))
