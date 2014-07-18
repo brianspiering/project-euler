1c5a3e3daaef23eede59ceee41a9d51fac838c7a
Brian Spiering <bspiering@gmail.com>
2014-07-18 10:18:44 -0700

    Brute force solution
    Fast enough!

--- python/problem_019.py
+++ python/problem_019.py
@@ -22,12 +22,18 @@ from calendar import day_name, monthrange
 
 start_year = 1901
 end_year = 2000
-weekdays = {i:day for i,day in enumerate(day_name)}
+weekdays = {day:i for i,day in enumerate(day_name)}
 
-def count_first_sundays():
-    return None
+def count_first_sundays(start_year, end_year):
+    "For each year and each month, find the first day of the month. Add to counter"
+    total_sundays = 0
+    for year in xrange(start_year, end_year+1):
+        for month in xrange(1,13):
+            if monthrange(year,month)[0] == weekdays["Sunday"]:
+                total_sundays += 1
+    return total_sundays
 
 if __name__ == "__main__":
     print("The number of Sundays falling on the first of the month "\
-            "during the twentieth century {0}."
-            .format(count_first_sundays()))
+            "during the twentieth century is {0}."
+            .format(count_first_sundays(start_year, end_year)))
