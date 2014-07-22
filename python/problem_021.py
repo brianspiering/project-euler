bcc4b1042bd2bec9838477e3f6fe3fd7f91a55fc
Brian Spiering <bspiering@gmail.com>
2014-07-21 20:05:57 -0700

    Add helper functions

--- python/problem_021.py
+++ python/problem_021.py
@@ -13,12 +13,27 @@ For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 a
 Evaluate the sum of all the amicable numbers under 10000.
 """
 
+from collections import Counter
+
 n = 10000
 
-def calc_something(n):
-    "Lorem"
-    return None
+def factors(n):
+    "Find factors of n."
+    return [_ for _ in xrange(1,n) if n % _ == 0]
+
+def make_factor_sum_dict(n):
+    "Make a dictionary of numbers (key) and sum of all factors (value) below n."
+    return {_:sum(factors(_)) for _ in xrange(1,n)}
+
+def sum_amicable_numbers(n):
+    "Find sum of amicable numbers below n."
+    factor_sum_dict = make_factor_sum_dict(n)
+    # Sum key value pairs where key and pairs are the same.
+    # eg:
+    # factor_sum_dict[220] = 284
+    # factor_sum_dict[284] = 220
+    return "'In Progress'"
 
 if __name__ == "__main__":
-    print("the sum of all the amicable numbers under {0} is {1}."
-            .format(n, calc_something(n)))
+    print("The sum of all the amicable numbers under {0} is {1}."
+            .format(n, sum_amicable_numbers(n)))
