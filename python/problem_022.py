2eac19ff50292ba3925dab245612b005e435c61b
Brian Spiering <bspiering@gmail.com>
2014-07-23 09:13:55 -0700

    Load names

--- python/problem_022.py
+++ python/problem_022.py
@@ -14,8 +14,19 @@ What is the total of all the name scores in the file?
 
 import string
 
+def process_file(file_name):
+     # Load file
+    with open(file_name) as f:
+        lines = f.read().split(",")
+
+    lines = [line[1:-1] for line in lines] # Strip extra set of quotes
+
+    return lines
+
 def total_names():
     "Find total of all the name scores."
+    names = process_file('names.txt') # Load names
+    names.sort() # Alphabeticalize
     return "'In Progress'"
 
 if __name__ == "__main__":
