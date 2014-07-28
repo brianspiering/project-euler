9d40cdb405aa637e318c9b35fbd3d82bb45e02dd
Brian Spiering <bspiering@gmail.com>
2014-07-28 13:56:06 -0700

    Change printing to be more pythonic

--- python/problem_024.py
+++ python/problem_024.py
@@ -16,7 +16,7 @@ What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 
 from __future__ import print_function
 import itertools
 
-digits = range(3) #  range(3) | range(10)
+digits = range(10) #  range(3) | range(10)
 
 # {n_elements (place_number, place_name}
 perm_picker = { 3: (6, 'sixth'),
@@ -27,11 +27,8 @@ def calc_something(digits):
 
 if __name__ == "__main__":
     print("The {} lexicographic permutation of the digits ".format(perm_picker[len(digits)][1]), end="")
-    for i, digit in enumerate(digits):
-        if i < len(digits)-2:
-            print("{}".format(digit), end=", ")
-        elif i == len(digits)-2:
-            print("{}".format(digit), end=", and ")
-        else:
-            print("{}".format(digit), end=" ")
+    for digit in digits[:-2]:
+        print("{}".format(digit), end=", ")
+    print("{}".format(digits[-2]), end=", and ")
+    print("{}".format(digits[-1]), end=" ")
     print("is {}.".format(calc_something(digits)))
