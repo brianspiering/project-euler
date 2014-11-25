c328ae030b6f62b29f017f3d79e43e6fc5c5abe8
Brian Spiering <bspiering@gmail.com>
2014-11-24 22:24:34 -0800

    Add support for number under 100

--- python/problem_017.py
+++ python/problem_017.py
@@ -9,12 +9,12 @@ If the numbers 1 to 5 are written out in words: one, two, three, four, five, the
 If all the numbers from 1 to 1000 (one thousand) inclusive were written out in words, how many letters would be used?
 """
 
-stop_number = 5 # 5 | 1000
+stop_number = 99 # 5 | 1000
 
 # Arabic Number: English Word
-number_dict = { 1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',
-            8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',13:'thirteen',
-            14:'fourteen',15:'fifteen',16:'sixteen',17:'seventeen',
+number_dict = {0: '', 1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',
+            7:'seven',8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',
+            13:'thirteen',14:'fourteen',15:'fifteen',16:'sixteen',17:'seventeen',
             18:'eighteen',19:'nineteen',20:'twenty',30:'thirty',40:'forty',
             50:'fifty',60:'sixty',70:'seventy',80:'eighty',90:'ninety',
             100:'hundred',1000:'thousand'}
@@ -27,15 +27,17 @@ def count_letters(stop_number, letter_count_dict):
     "Count number of letters in words from 1 to n"
     sum_letters = 0
     for n in range(1,stop_number+1):
-        if len(str(n)) == 1:
+        if (len(str(n)) == 1) | n <= 20:
             sum_letters += letter_count_dict[n]
-        elif len(str(n)) == 2:
-            pass
+        elif (len(str(n)) == 2) | n > 20:
+            sum_letters += sum([letter_count_dict[int(character+'0')] 
+                                if i == 0 
+                                else letter_count_dict[int(character)]
+                                for i, character in enumerate(str(n))])    
         elif len(str(n)) == 3:
             pass
-
     return sum_letters
-    
+
 if __name__ == "__main__":
     letter_count_dict = gen_letter_count(number_dict)
     print("The number of letters in the numbers from 1 to {0} is {1}."
