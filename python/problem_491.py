35aff0cab5b9c9fcb70b41541d41f42f796cca21
Brian Spiering <bspiering@gmail.com>
2014-12-07 20:30:10 -0800

    Remove typos

--- python/problem_491.py
+++ python/problem_491.py
@@ -16,10 +16,10 @@ def n_double_pandigital_mod_11():
     """"Find the number of double pandigital numbers are divisible by 11:
             1. Create an interator object of all permutations.
             2. Convert each tuple to a integer.
-            3. Sum integers that are modulus 11 with list comprehesion."""
+            3. Sum integers that are modulus 11 with list comprehension."""
     double_pandigitals = permutations(range(10)+range(10))
-    return sum([reduce(lambda n1, n2: n1*10+n2, current_tuple) % 11 == 0 
-                for current_tuple in double_pandigitals])
+    return sum(reduce(lambda n1, n2: n1*10+n2, current_tuple) % 11 == 0 
+                for current_tuple in double_pandigitals)
 
 if __name__ == "__main__":
     print("The number of double pandigital numbers are divisible by 11 is: {0}\n"
