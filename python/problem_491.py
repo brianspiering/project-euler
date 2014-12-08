05ff65b6a6113f8c3d81b84328932af0dffbfc28
Brian Spiering <bspiering@gmail.com>
2014-12-07 19:17:54 -0800

    Replace str join clause with reduce function

--- python/problem_491.py
+++ python/problem_491.py
@@ -18,7 +18,8 @@ def n_double_pandigital_mod_11():
             2. Convert each tuple to a integer.
             3. Sum integers that are modulus 11 with list comprehesion."""
     double_pandigitals = permutations(range(10)+range(10))
-    return sum([int(''.join(str(x) for x in current_tuple)) % 11 == 0 for current_tuple in double_pandigitals])
+    return sum([reduce(lambda n1, n2: n1*10+n2, current_tuple) % 11 == 0 
+                for current_tuple in double_pandigitals])
 
 if __name__ == "__main__":
     print("The number of double pandigital numbers are divisible by 11 is: {0}\n"
