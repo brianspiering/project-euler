fe3744b0dd56d3e4517159df0663c5d54f2ee04c
Brian Spiering <bspiering@gmail.com>
2014-12-07 20:50:07 -0800

    Start another possible method to solve it

--- python/problem_491.py
+++ python/problem_491.py
@@ -10,7 +10,7 @@ We call a positive integer double pandigital if it uses all the digits 0 to 9 ex
 How many double pandigital numbers are divisible by 11?
 """
 
-from itertools import permutations
+from itertools import permutations, izip
 
 def n_double_pandigital_mod_11():
     """"Find the number of double pandigital numbers are divisible by 11:
@@ -18,6 +18,12 @@ def n_double_pandigital_mod_11():
             2. Convert each tuple to a integer.
             3. Sum integers that are modulus 11 with list comprehension."""
     double_pandigitals = permutations(range(10)+range(10))
+
+    # NOTE: It might be faster to elimate many contenders by ...
+    # Take the alternating sum of the digits from left to right. 
+    # If that is divisible by 11, so is the original number.
+    # (sum(int(e) for e in str(n)[::2]) - sum(int(e) for e in str(n)[1::2])) % 11 == 0
+
     return sum(reduce(lambda n1, n2: n1*10+n2, current_tuple) % 11 == 0 
                 for current_tuple in double_pandigitals)
 
