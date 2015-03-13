cc980a77fa6ae3a9a6f695abd81af67922d73109
Brian Spiering <bspiering@gmail.com>
2015-03-13 05:42:42 -0700

    Improve code readability

--- python/problem_007.py
+++ python/problem_007.py
@@ -7,14 +7,15 @@ http://projecteuler.net/problem=7
 
 By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 
-What is the 10 001st prime number?
+What is the 10,001st prime number?
 """
 
 from itertools import islice
 
 n = 10001 # 6 | 10001
 
-ordinal_number_suffixes = {6:'th', 10001:'st'}
+ordinal_number_suffixes = {6:'th', 
+                           10001:'st'}
 
 def gen_primes():
     "A generator function for prime numbers"
@@ -34,5 +35,7 @@ def nth(iterable, n, default=None):
     return next(islice(iterable, n, None), default)
 
 if __name__ == "__main__":
-    print("The {0}{1} prime number is {2}."
-          .format(n, ordinal_number_suffixes[n], nth(gen_primes(), n-1)))
+    print("The {0:,}{1} prime number is {2}."
+          .format(n,
+                  ordinal_number_suffixes[n],
+                  nth(gen_primes(), n-1)))
